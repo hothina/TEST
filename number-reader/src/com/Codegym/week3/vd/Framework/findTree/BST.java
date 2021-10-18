e67f4a35676f1ad297eb9cc7dcea15d8f99e1cf5
hothina <88428484+hothina@users.noreply.github.com>
2021-10-18 09:40:01 +0700

    bt

--- number-reader/src/com/Codegym/week3/vd/Framework/findTree/BST.java
+++ number-reader/src/com/Codegym/week3/vd/Framework/findTree/BST.java
@@ -17,12 +17,48 @@ public class BST<E extends Comparable<E>> extends AbtractTree<E> {
     @Override
     public boolean insert(E e) {
         if (root == null)
-            root = creteNewNode(e);
-        return false;
+            root = createNewNode(e); /*create a new root*/
+        else {
+            /*locate the parent node*/
+            TreeNode<E> parent = null;
+            TreeNode<E> current = root;
+            while (current != null) {
+                if (e.compareTo(current.element) < 0) {
+                    parent = current;
+                    current = current.left;
+                } else if (e.compareTo(current.element) > 0) {
+                    parent = current;
+                    current = current.right;
+                } else
+                    return false; /*Duplicate node not inserted*/
+            }
+            if (e.compareTo(parent.element) < 0)
+                parent.left = createNewNode(e);
+            else
+                parent.right = createNewNode(e);
+        }
+        size++;
+        return true; /*element inserted successfully*/
+    }
+
+    protected TreeNode<E> createNewNode(E e) {
+        return new TreeNode<>(e);
     }
 
     @Override
     public int getSize() {
-        return 0;
+        return size;
+    }
+
+    @Override
+    public void inorder() {
+        inorder(root);
+    }
+
+    protected void inorder(TreeNode<E> root) {
+        if (root == null) return;
+        inorder(root.left);
+        System.out.println(root.element + " ");
+        inorder(root.right);
     }
 }
